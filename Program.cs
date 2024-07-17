cdf68bfeb4884107bbd38a2e453075841b059191
imranzonger <imranatzong11@gmail.com>
2024-07-17 19:50:37 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using bankProject;
+
+GoodBankAccount objGoodBA = new GoodBankAccount(); ;
+objGoodBA.OpenAccount();
+objGoodBA.Deposit();
+objGoodBA.Withdraw();
+objGoodBA.Balance();
+objGoodBA.CloseAccount();
